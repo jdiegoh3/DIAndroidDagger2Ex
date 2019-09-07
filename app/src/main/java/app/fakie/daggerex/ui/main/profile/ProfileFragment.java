5e431147ee78fb36cbffc7d4daecbeb5af5c36d8
Juan Diego H <jdiegoh3@gmail.com>
2019-09-07 15:41:33 -0500

    Created observers into ProfileFragment.
    - Create observers management careful with fragments life cycle.
    - Removing observers tied to determinate life cycle owner with removeObservers method.

--- app/src/main/java/app/fakie/daggerex/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/app/fakie/daggerex/ui/main/profile/ProfileFragment.java
@@ -5,14 +5,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import javax.inject.Inject;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import app.fakie.daggerex.R;
+import app.fakie.daggerex.models.User;
+import app.fakie.daggerex.ui.auth.AuthResource;
 import app.fakie.daggerex.viewmodels.ViewModelProviderFactory;
 import dagger.android.support.DaggerFragment;
 
@@ -21,6 +25,8 @@ public class ProfileFragment extends DaggerFragment {
     private static final String TAG = ProfileFragment.class.getSimpleName();
 
     private ProfileViewModel viewModel;
+
+    private TextView email, username, website;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -34,6 +40,45 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onViewCreated: Profile fragment was created..");
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
         viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+        
+        subscribeObservers();
+    }
+
+    public void subscribeObservers(){
+        // Remove previous observers before create a new ones.
+        // Why ? Because the Fragments have their owns life cycle and can be destroyed and recreated
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if(userAuthResource != null){
+                    switch (userAuthResource.status){
+                        case AUTHENTICATED:{
+                            setUserDetails(userAuthResource.data);
+                            break;
+                        }
+                        case ERROR:{
+                            setErrorDetails(userAuthResource.message);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void setErrorDetails(String message) {
+        email.setText(message);
+    }
+
+    private void setUserDetails(User data) {
+        email.setText(data.getEmail());
+        username.setText(data.getUsername());
+        website.setText(data.getWebsite());
+
     }
 }
--- app/src/main/java/app/fakie/daggerex/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/app/fakie/daggerex/ui/main/profile/ProfileViewModel.java
@@ -4,14 +4,27 @@ import android.util.Log;
 
 import javax.inject.Inject;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+import app.fakie.daggerex.SessionManager;
+import app.fakie.daggerex.models.User;
+import app.fakie.daggerex.ui.auth.AuthResource;
 
 public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "ProfileViewModel";
 
+    private SessionManager sessionManager;
+
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: ViewModel for ProfileFragment is ready.. c:");
+        Log.d(TAG, "ProfileViewModel: " + sessionManager.getAuthUser().getValue().data.getEmail());
     }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return sessionManager.getAuthUser();
+    }
+
 }
